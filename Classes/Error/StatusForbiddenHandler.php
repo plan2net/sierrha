576fe8202e31de940c0ad39c08b56214c02b6350
Wolfgang Klinger <wk@plan2.net>
2019-02-11 08:55:19 +0100

    [BUGFIX] Reformat code (PSR-2)
    Add missing author’s ‘name’ property in composer.json

--- Classes/Error/StatusForbiddenHandler.php
+++ Classes/Error/StatusForbiddenHandler.php
@@ -37,130 +37,130 @@ use TYPO3\CMS\Frontend\Page\PageAccessFailureReasons;
 class StatusForbiddenHandler implements PageErrorHandlerInterface
 {
 
-	/**
-	 * @var int
-	 */
-	protected $statusCode;
+    /**
+     * @var int
+     */
+    protected $statusCode;
 
-	/**
-	 * @var array
-	 */
-	protected $handlerConfiguration;
+    /**
+     * @var array
+     */
+    protected $handlerConfiguration;
 
-	/**
-	 * @param int $statusCode
-	 * @param array $configuration
-	 */
-	public function __construct(int $statusCode, array $configuration)
-	{
-		$this->statusCode = $statusCode;
-		$this->handlerConfiguration = $configuration;
-	}
+    /**
+     * @param int $statusCode
+     * @param array $configuration
+     */
+    public function __construct(int $statusCode, array $configuration)
+    {
+        $this->statusCode = $statusCode;
+        $this->handlerConfiguration = $configuration;
+    }
 
-	/**
-	 * @param ServerRequestInterface $request
-	 * @param string $message
-	 * @param array $reasons
-	 * @return ResponseInterface
-	 * @throws \Exception
-	 */
-	public function handlePageError(
-		ServerRequestInterface $request,
-		string $message,
-		array $reasons = []
-	): ResponseInterface {
-		$extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('sierrha');
-		$resolvedUrl = '/';
-		try {
-			if ($this->statusCode !== 403) {
-				throw new \InvalidArgumentException('StatusForbiddenHandler only handles status 403.', 1547651137);
-			}
-			if (empty($this->handlerConfiguration['tx_sierrha_loginPage'])) {
-				throw new \InvalidArgumentException('StatusForbiddenHandler needs to have a URL set.', 1547651257);
-			}
+    /**
+     * @param ServerRequestInterface $request
+     * @param string $message
+     * @param array $reasons
+     * @return ResponseInterface
+     * @throws \Exception
+     */
+    public function handlePageError(
+        ServerRequestInterface $request,
+        string $message,
+        array $reasons = []
+    ): ResponseInterface {
+        $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('sierrha');
+        $resolvedUrl = '/';
+        try {
+            if ($this->statusCode !== 403) {
+                throw new \InvalidArgumentException('StatusForbiddenHandler only handles status 403.', 1547651137);
+            }
+            if (empty($this->handlerConfiguration['tx_sierrha_loginPage'])) {
+                throw new \InvalidArgumentException('StatusForbiddenHandler needs to have a URL set.', 1547651257);
+            }
 
-			// if the user is already logged in, another login with the same account will not resolve the issue
-			// NOTE: we're checking also for BE sessions in case a FE user group is simulated
-			$context = GeneralUtility::makeInstance(Context::class);
-			if ($context->getPropertyFromAspect('frontend.user', 'isLoggedIn')) {
-				if (!empty($this->handlerConfiguration['tx_sierrha_missingPermissionsPage'])) {
-					$resolvedUrl = $this->resolveUrl($request,
-						$this->handlerConfiguration['tx_sierrha_missingPermissionsPage']);
-				} elseif ($context->getPropertyFromAspect('backend.user', 'isLoggedIn')
-					&& $context->getPropertyFromAspect('frontend.user',
-						'groupIds')[1] === -2 // special "any group" (simulated)
-				) {
-					return GeneralUtility::makeInstance(ErrorController::class)->pageNotFoundAction(
-						$request,
-						'The requested page was not accessible with provided credentials',
-						['code' => PageAccessFailureReasons::ACCESS_DENIED_GENERAL]
-					);
-				}
-			} else {
-				$resolvedUrl = $this->resolveUrl($request, $this->handlerConfiguration['tx_sierrha_loginPage']);
-			}
-		} catch (\Exception $e) {
-			if ($extensionConfiguration['debugMode']
-				|| GeneralUtility::cmpIP(GeneralUtility::getIndpEnv('REMOTE_ADDR'),
-					$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'])) {
-				$content = GeneralUtility::makeInstance(ErrorPageController::class)->errorAction(
-					get_class($e),
-					$e->getMessage(),
-					AbstractMessage::ERROR,
-					$e->getCode()
-				);
+            // if the user is already logged in, another login with the same account will not resolve the issue
+            // NOTE: we're checking also for BE sessions in case a FE user group is simulated
+            $context = GeneralUtility::makeInstance(Context::class);
+            if ($context->getPropertyFromAspect('frontend.user', 'isLoggedIn')) {
+                if (!empty($this->handlerConfiguration['tx_sierrha_missingPermissionsPage'])) {
+                    $resolvedUrl = $this->resolveUrl($request,
+                        $this->handlerConfiguration['tx_sierrha_missingPermissionsPage']);
+                } elseif ($context->getPropertyFromAspect('backend.user', 'isLoggedIn')
+                    && $context->getPropertyFromAspect('frontend.user',
+                        'groupIds')[1] === -2 // special "any group" (simulated)
+                ) {
+                    return GeneralUtility::makeInstance(ErrorController::class)->pageNotFoundAction(
+                        $request,
+                        'The requested page was not accessible with provided credentials',
+                        ['code' => PageAccessFailureReasons::ACCESS_DENIED_GENERAL]
+                    );
+                }
+            } else {
+                $resolvedUrl = $this->resolveUrl($request, $this->handlerConfiguration['tx_sierrha_loginPage']);
+            }
+        } catch (\Exception $e) {
+            if ($extensionConfiguration['debugMode']
+                || GeneralUtility::cmpIP(GeneralUtility::getIndpEnv('REMOTE_ADDR'),
+                    $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'])) {
+                $content = GeneralUtility::makeInstance(ErrorPageController::class)->errorAction(
+                    get_class($e),
+                    $e->getMessage(),
+                    AbstractMessage::ERROR,
+                    $e->getCode()
+                );
 
-				return new HtmlResponse($content, 500);
-			}
-			throw $e;
-		}
+                return new HtmlResponse($content, 500);
+            }
+            throw $e;
+        }
 
-		/* @var $siteLanguage SiteLanguage */
-		$siteLanguage = $request->getAttribute('language');
-		$resolvedUrl = str_replace(
-			['###ISO_639-1###', '###IETF_BCP47'],
-			[$siteLanguage->getTwoLetterIsoCode(), $siteLanguage->getHreflang()],
-			$resolvedUrl
-		);
-		$requestUri = (string)$request->getUri();
-		$loginParameters = str_replace(
-			['###URL###', '###URL_BASE64###'],
-			[rawurlencode($requestUri), rawurlencode(base64_encode($requestUri))],
-			$this->handlerConfiguration['tx_sierrha_loginUrlParameter']
-		);
+        /* @var $siteLanguage SiteLanguage */
+        $siteLanguage = $request->getAttribute('language');
+        $resolvedUrl = str_replace(
+            ['###ISO_639-1###', '###IETF_BCP47'],
+            [$siteLanguage->getTwoLetterIsoCode(), $siteLanguage->getHreflang()],
+            $resolvedUrl
+        );
+        $requestUri = (string)$request->getUri();
+        $loginParameters = str_replace(
+            ['###URL###', '###URL_BASE64###'],
+            [rawurlencode($requestUri), rawurlencode(base64_encode($requestUri))],
+            $this->handlerConfiguration['tx_sierrha_loginUrlParameter']
+        );
 
-		return new RedirectResponse($resolvedUrl . (strpos($resolvedUrl,
-				'?') === false ? '?' : '&') . $loginParameters);
-	}
+        return new RedirectResponse($resolvedUrl . (strpos($resolvedUrl,
+                '?') === false ? '?' : '&') . $loginParameters);
+    }
 
-	/**
-	 * @param ServerRequestInterface $request
-	 * @param string $typoLinkUrl
-	 * @return string
-	 * @throws \TYPO3\CMS\Core\Routing\InvalidRouteArgumentsException
-	 */
-	protected function resolveUrl(ServerRequestInterface $request, string $typoLinkUrl): string
-	{
-		$linkService = GeneralUtility::makeInstance(LinkService::class);
-		$urlParams = $linkService->resolve($typoLinkUrl);
-		if ($urlParams['type'] !== 'page' && $urlParams['type'] !== 'url') {
-			throw new \InvalidArgumentException('StatusForbiddenHandler can only handle TYPO3 links of type "page" or "url"',
-				1547651754);
-		}
-		if ($urlParams['type'] === 'url') {
-			return $urlParams['url'];
-		}
+    /**
+     * @param ServerRequestInterface $request
+     * @param string $typoLinkUrl
+     * @return string
+     * @throws \TYPO3\CMS\Core\Routing\InvalidRouteArgumentsException
+     */
+    protected function resolveUrl(ServerRequestInterface $request, string $typoLinkUrl): string
+    {
+        $linkService = GeneralUtility::makeInstance(LinkService::class);
+        $urlParams = $linkService->resolve($typoLinkUrl);
+        if ($urlParams['type'] !== 'page' && $urlParams['type'] !== 'url') {
+            throw new \InvalidArgumentException('StatusForbiddenHandler can only handle TYPO3 links of type "page" or "url"',
+                1547651754);
+        }
+        if ($urlParams['type'] === 'url') {
+            return $urlParams['url'];
+        }
 
-		/* @var $site Site */
-		$site = $request->getAttribute('site', null);
-		if (!$site instanceof Site) {
-			$site = GeneralUtility::makeInstance(SiteFinder::class)->getSiteByPageId((int)$urlParams['pageuid']);
-		}
+        /* @var $site Site */
+        $site = $request->getAttribute('site', null);
+        if (!$site instanceof Site) {
+            $site = GeneralUtility::makeInstance(SiteFinder::class)->getSiteByPageId((int)$urlParams['pageuid']);
+        }
 
-		return (string)$site->getRouter()->generateUri(
-			(int)$urlParams['pageuid'],
-			['_language' => $request->getAttribute('language', null)]
-		);
-	}
+        return (string)$site->getRouter()->generateUri(
+            (int)$urlParams['pageuid'],
+            ['_language' => $request->getAttribute('language', null)]
+        );
+    }
 
 }
